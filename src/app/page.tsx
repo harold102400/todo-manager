6a1cccedf19c518a867f0ffa8303bb256b4ab96a
hmontes <haroldjosehd24@gmail.com>
2025-05-06 15:08:25 -0400

    fix: update base route to include new Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-  return (<></>);
+import Home from "@/components/home/Home";
+
+export default function HomePage() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 }
